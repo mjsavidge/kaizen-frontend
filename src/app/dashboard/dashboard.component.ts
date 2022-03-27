e35cd5d388a42402154b3208c4fcb8232e37c796
mjsavidge <67027226+mjsavidge@users.noreply.github.com>
2022-03-26 22:29:22 -0400

    working on upload tab in dashboard. sorted links for navigation and added dividers

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,22 +14,9 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 })
 export class DashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
-  file: null;
-  profileUrl: any;
- 
 
   constructor(private breakpointObserver: BreakpointObserver,public authService: AuthService, public storage: AngularFireStorage) {}
 
-  uploadVideo(event, uploader: string,  title: string, description: string, uploadTime:string){
-    const user = this.authService.userData;
-    const file = event.target.files[0];
-    this.storage.upload('user/' + user.uid + '/videos/' + title, file, {customMetadata:{ uploader: uploader, title: title, description: description, time: uploadTime}});
-  }
-
-  download(){
-    const user = this.authService.userData;
-    const ref = this.storage.ref('user/' + user.uid + '/profile.png');
-    return this.profileUrl = ref.getDownloadURL;
-  }
+  
  
 }
